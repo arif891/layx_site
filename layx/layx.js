7882146e33f20b68d7e0d47a24e444c6ff47652f
Arif <arif.hossain.9213@gmail.com>
2024-10-26 14:29:59 +0600

    fix

--- layx/layx.js
+++ layx/layx.js
@@ -3,11 +3,8 @@ import Theme from "./others/theme/theme.js";
 
 /* Components */
 import Navbar from "./components/navbar/navbar.js";
-import Dialog from "./components/dialog/dialog.js";
-import {Sheet} from "./components/sheet/sheet.js";
 
 /* Other */
-import CodeHighlighter from "./others/syntax_highlighter/syntax_highlighter.js";
 
 const mediaQuery = window.matchMedia('(max-width: 992px)');
 
