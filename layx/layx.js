0263d9ab50c00699dc622d03e78ee349e2e47504
Arif <arif.hossain.9213@gmail.com>
2024-10-26 14:30:57 +0600

    fix

--- layx/layx.js
+++ layx/layx.js
@@ -3,6 +3,7 @@ import Theme from "./others/theme/theme.js";
 
 /* Components */
 import Navbar from "./components/navbar/navbar.js";
+import {Sheet} from "./components/sheet/sheet.js";
 
 /* Other */
 
